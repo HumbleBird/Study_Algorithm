8e756a83d630783ff06bbafdae1b1fd11a17b595
HumbleBird <wltnsdid67@naver.com>
2023-12-17 14:49:34 +0900

    Initial commit

---
+++ Algorithm_02/PriorityQueue.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Algorithm_02
+{
+    class TreeNode<T>
+    {
+        public T Data { get; set; }
+        public List<TreeNode<T>> Children { get; set; } = new List<TreeNode<T>>();
+
+
+        static TreeNode<string> MakeTree()
+        {
+            TreeNode<string> root = new TreeNode<string>() { Data = "R1 개발실" };
+            {
+                {
+                    TreeNode<string> node = new TreeNode<string>() { Data = "디자인팀" };
+                    node.Children.Add(new TreeNode<string>() { Data = "전투" });
+                    node.Children.Add(new TreeNode<string>() { Data = "경제" });
+                    node.Children.Add(new TreeNode<string>() { Data = "스토리" });
+                    root.Children.Add(node);
+                }
+                {
+                    TreeNode<string> node = new TreeNode<string>() { Data = "프로그랭밍팀" };
+                    node.Children.Add(new TreeNode<string>() { Data = "서버" });
+                    node.Children.Add(new TreeNode<string>() { Data = "클라" });
+                    node.Children.Add(new TreeNode<string>() { Data = "엔진" });
+                    root.Children.Add(node);
+                }
+                {
+                    TreeNode<string> node = new TreeNode<string>() { Data = "아트팀" };
+                    node.Children.Add(new TreeNode<string>() { Data = "배경" });
+                    node.Children.Add(new TreeNode<string>() { Data = "캐릭터" });
+                    root.Children.Add(node);
+                }
+
+
+            }
+
+            return root;
+        }
+
+        static void PrintTree(TreeNode<string> root)
+        {
+            Console.WriteLine(root.Data);
+
+            foreach (TreeNode<string> c in root.Children)
+            {
+                PrintTree(c);
+            }
+        }
+
+        static int GetHeight(TreeNode<string> root)
+        {
+            int height = 0;
+
+            foreach (TreeNode<string> c in root.Children)
+            {
+                int newHeight = GetHeight(c) + 1;
+                if (height < newHeight)
+                    height = newHeight;
+            }
+
+            return height;
+        }
+    }
+
+    class PriorityQueue<T> where T : IComparable<T>
+    {
+        List<T> _heap = new List<T>();
+
+        public void Push(T data)
+        {
+            _heap.Add(data);
+
+            int now = _heap.Count - 1;
+            while (now > 0)
+            {
+                int next = (now - 1) / 2;
+                if (_heap[now].CompareTo(_heap[next]) < 0)
+                    break;
+
+                T temp = _heap[now];
+                _heap[now] = _heap[next];
+                _heap[next] = temp;
+
+                now = next;
+            }
+        }
+
+
+        public T Pop()
+        {
+            T ret = _heap[0];
+
+            int lastIndex = _heap.Count - 1;
+            _heap[0] = _heap[lastIndex];
+            _heap.RemoveAt(lastIndex);
+            lastIndex--;
+
+            int now = 0;
+            while (true)
+            {
+                int left = 2 * now + 1;
+                int right = 2 * now + 2;
+
+                int next = now;
+                if (left <= lastIndex && _heap[next].CompareTo(_heap[left]) < 0)
+                    next = left;
+                if (right <= lastIndex && _heap[next].CompareTo(_heap[right]) < 0)
+                    next = right;
+
+                if (next == now)
+                    break;
+
+                T temp = _heap[now];
+                _heap[now] = _heap[next];
+                _heap[next] = temp;
+
+                now = next;
+            }
+
+            return ret;
+        }
+
+        public int Count()
+        {
+            return _heap.Count;
+
+        }
+
+    }
+}
---
+++ StackQueue_Excirsize/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StackQueue_Exercise
+{
+    class Graph
+    {
+        int[,] adj = new int[6, 6]
+        {
+            { -1, 15, -1, 35, -1, -1 },
+            { 15, -1, 5, 10, -1, -1 },
+            { -1, 5, -1, -1, -1, -1 },
+            { 35, 10, -1, -1, 5, -1 },
+            { -1, -1, -1, 5, -1, 5 },
+            { -1, -1, -1, -1, 5, -1 }
+        };
+
+        public void Dijikstra(int start)
+        {
+            bool[] visited = new bool[6];
+            int[] distance = new int[6];
+            int[] parent = new int[6];
+
+            Array.Fill(distance, Int32.MaxValue);
+
+            distance[start] = 0;
+            parent[start] = start;
+
+            while (true)
+            {
+                int closest = Int32.MaxValue;
+                int now = -1;
+
+                for (int i = 0; i < 6; i++)
+                {
+                    if (visited[i])
+                        continue;
+
+                    if (distance[i] == Int32.MaxValue || distance[i] >= closest)
+                        continue;
+
+                    closest = distance[i];
+                    now = i;
+                }
+
+                if (now == -1)
+                    break;
+                visited[now] = true;
+
+                for (int next = 0; next < 6; next++)
+                {
+                    if (adj[now, next] == -1)
+                        continue;
+
+                    if (visited[next])
+                        continue;
+
+                    int nextDist = distance[now] + adj[now, next] ;
+
+                    if(nextDist < distance[next])
+                    {
+                        distance[next] = nextDist;
+                         parent[next] = now;
+                    }
+                }
+            }
+        }
+
+        public void BFS(int start)
+        {
+            bool[] found = new bool[6];
+            
+            Queue<int> q = new Queue<int>();
+            q.Enqueue(start);
+            found[start] = true;
+            while (q.Count > 0)
+            {
+                int now = q.Dequeue();
+                Console.WriteLine(now);
+
+                for (int next = 0; next < 6; next++)
+                {
+                    if (adj[now, next] == 0)
+                        continue;
+                    if (found[next])
+                        continue;
+                    q.Enqueue(next);
+                    found[next] = true;
+                }
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Graph graph = new Graph();
+            graph.Dijikstra(0);
+        }
+    }
+}
